101598697c946efa41e8343ab86b5a79121c0696
Saur03 <chawlasaurabh97@gmail.com>
2024-03-22 18:52:22 -0400

    Modified add score version 2 file, now it is running fine

--- Assignment11A of PHP/addscore_V2.php
+++ Assignment11A of PHP/addscore_V2.php
@@ -1,48 +1,45 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Yatzy - High Scores</title>
+<title>Yatzy - Add Your High Scores</title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <body>
 <h2>Yatzy - Add Your High Scores</h2>
 <?php
 require_once('connectvars.php');
-$TableName = "high_scores";
-define('UPLOADPATH', 'images/');
-define('DISPLAYPATH', 'images/');
-define('MAXFILESIZE', 32768);
+require_once('appvars.php');
 if (isset($_POST['submit'])) {
-	$name = $_POST['name'];
-	$score = $_POST['score'];
-	$screenshot = $_FILES['screenshot']['name'];
-	$screenshot_type = $_FILES['screenshot']['type'];
-	$screenshot_size = $_FILES['screenshot']['size'];
-	
+$name = $_POST['name'];
+$score = $_POST['score'];
+$screenshot = $_FILES['screenshot']['name'];
+$screenshot_type = $_FILES['screenshot']['type'];
+$screenshot_size = $_FILES['screenshot']['size'];
+
 if (!empty($name) && !empty($score) && !empty($screenshot)) {
-	$target = UPLOADPATH.$screenshot;
+	$target = UPLOADPATH . $screenshot;
+	
 	if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target)) {
+		$TableName = "high_scores";
 		$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-		
 		$query = "INSERT INTO high_scores VALUES (0, NOW(), '$name', '$score', '$screenshot')";
 		mysqli_query($dbc, $query);
 		echo '<p>Thanks for adding your new high score!</p>';
 		echo '<p><strong>Name:</strong>' . $name . '<br/>';
 		echo '<strong>Score:</strong>' . $score . '<br />';
-		echo '<img src = "' . "images/mariascore.jpg" . $screenshot . '" alt = "Score image" /></p>';
+		echo '<img src = "' . DISPLAYPATH . $screenshot . '" alt = "Score image" /></p>';
 		
-		echo '<p><a href = "yatzyIndex_v2.php">&lt; &lt; Back to high scores</a></p>';
-		
+		echo '<p><a href = "yatzyIndex_v3.php">&lt; &lt; Back to high scores</a></p>';
 		$name = "";
 		$score = "";
 		$screenshot = "";
-		
+			
 		mysqli_close($dbc);
-	}
-	else {
-		echo '<p class="error">Sorry, there was a problem uploading your screenshot image.</p>';
-}
-}
+		}
+		else {
+			echo '<p class="error">Sorry, there was a problem uploading your screenshot image.</p>';
+			}
+			}
 else {
 	echo '<p class = "error">Please enter all of the information to add your high score.</p>';
 }
@@ -62,4 +59,5 @@ else {
 </form>
 </body> 
 </html>
-
+		
+		
